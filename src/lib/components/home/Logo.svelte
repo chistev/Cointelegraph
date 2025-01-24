c2a03c37a69f51b4e07e524dfec33b95a2a14f92
chistev <stephenowabie@gmail.com>
2025-01-24 12:57:05 +0100

    added Logo component

---
+++ src/lib/components/home/Logo.svelte
@@ -0,0 +1,23 @@
+  <div class="logo">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" fill="white">
+      <circle cx="50" cy="50" r="40" />
+      <text x="50%" y="50%" text-anchor="middle" dy=".3em" font-size="20" fill="#000">
+        CT
+      </text>
+    </svg>
+    <span>COINTELEGRAPH</span>
+  </div>
+  
+  <style>
+    .logo {
+      display: flex;
+      align-items: center;
+      color: #f7c32d;
+    }
+  
+    .logo svg {
+      height: 40px;
+      margin-right: 10px;
+    }
+  </style>
+  
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import Logo from "$lib/components/home/Logo.svelte";
+
+</script>
 <style>
     .header {
         display: flex;
@@ -6,17 +10,6 @@
         background-color: #111;
         padding: 10px 20px;
         color: #fff;
-    }
-
-    .logo {
-        display: flex;
-        align-items: center;
-        color:#f7c32d
-    }
-
-    .logo svg {
-        height: 40px;
-        margin-right: 10px;
     }
 
     .right-section {
@@ -153,15 +146,7 @@
 </style>
 
 <div class="header">
-    <div class="logo">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" fill="white">
-            <circle cx="50" cy="50" r="40" />
-            <text x="50%" y="50%" text-anchor="middle" dy=".3em" font-size="20" fill="#000">
-                CT
-            </text>
-        </svg>
-        <span>COINTELEGRAPH</span>
-    </div>
+    <Logo/>
 
     <div class="right-section">
         <div class="social-icons">
